101e8b6736b68ff668610c8cda0ff26276b3ea97
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-01-07 05:49:45 +0530

    FavIcon Updated

--- src/App.js
+++ src/App.js
@@ -4,44 +4,42 @@
 // import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
-import React , {useState} from "react";
+import React, { useState } from "react";
 import Alert from "./Components/Alert";
 function App() {
-  const [mode,setMode]=useState('light');
-  const [alert,setAlert]= useState(null);
-  const showAlert=(message,type)=>{
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
     });
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null);
-    },1500);
+    }, 1500);
   }
-  const toggleMode=()=>{
-    if(mode==='dark')
-    {
+  const toggleMode = () => {
+    if (mode === 'dark') {
       setMode('light');
-      document.body.style.backgroundColor='white';
-      showAlert("Light Mode Enabled","success");
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode Enabled", "success");
     }
-    else
-    {
-      setMode('dark');  
-      document.body.style.backgroundColor='gray';
-      showAlert("Dark Mode Enabled","success");
+    else {
+      setMode('dark');
+      document.body.style.backgroundColor = 'gray';
+      showAlert("Dark Mode Enabled", "success");
     }
   };
   return (
     <>
-    <Navbar titlename="TextUtils" mode={mode} toggleMode={toggleMode}/>
-    <Alert alert={alert}/>
-    <div className="container " >
-    <TextForm heading="Enter Text to Analyze here" mode={mode} showAlert={showAlert}/>
-    {/* <About heading="About Us" /> */}
-    
-    </div>
-  </>
+      <Navbar titlename="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container " >
+        <TextForm heading="Enter Text to Analyze here" mode={mode} showAlert={showAlert} />
+        {/* <About heading="About Us" /> */}
+
+      </div>
+    </>
   );
 }
 
--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export default function Alert(props) {
-  const capitalize=(word)=>{
-    let str=word.charAt(0).toUpperCase() + word.slice(1);
+  const capitalize = (word) => {
+    let str = word.charAt(0).toUpperCase() + word.slice(1);
     return str;
   };
-    return (
+  return (
     props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-     <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
-</div>
-    
+      <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
+    </div>
+
   )
 }
